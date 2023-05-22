7823c02f02bbc30cfd16106103d9ce176243db12
cygnu <crucia8@icloud.com>
2023-05-22 23:50:20 +0900

    feat: Display sample images in lib/screens/main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -14,7 +14,8 @@ class MainScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Card(
-              child: Image.network(''),
+              child: Image.network(
+                  'https://www.kyoritsu-biyo.com/column/wordpress/wp-content/uploads/2022/03/%E4%BA%8C%E3%81%AE%E8%85%95%E3%81%AE%E3%82%B5%E3%82%A4%E3%82%BA%E3%82%92%E6%B8%AC%E3%82%8B%E5%A5%B3%E6%80%A7.jpg'),
             ),
             const Text('腕'),
           ],
